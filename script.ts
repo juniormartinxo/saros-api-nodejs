d276df3240bd757e1a25ae9cfbab868b306925d9
Junior Martins <amjr.box@gmail.com>
2022-01-14 08:08:27 -0300

    up

--- script.ts
+++ script.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 //import moment from 'moment'
-import formatISO from 'date-fns/formatISO'
+//import formatISO from 'date-fns/formatISO'
+import parseISO from 'date-fns/parseISO'
+import { format } from 'date-fns'
 
 const prisma = new PrismaClient()
 
@@ -26,18 +28,34 @@ async function main() {
   })
   */
 
-  const birth = new Date('2022-01-01')
-  const dateT = formatISO(birth)
+  const birth = '2022-01-01T00:00:00.000Z'
+  //const dateT = formatISO(birth)
+
+  const dateFormat = parseISO(birth)
+
+  //const dateT = format(new Date(formatISO(new Date(birth))), 'yyyy-MM-dd')
 
   //const dateR = new Date('2022-01-01')
 
-  console.log(dateT)
+  console.log('dateFormat', birth.substring(0, 10))
+
+  //console.log('dateT', dateT)
+
+  function isoDate2Date(isoDate: string) {
+    return (
+      isoDate.substring(0, 4) +
+      '-' +
+      isoDate.substring(5, 7) +
+      '-' +
+      isoDate.substring(8, 10)
+    )
+  }
 
   const timesheets = await prisma.timesheets.create({
     data: {
       timesheet_teacher_id: '09671dee-49c1-400f-8431-046571784609',
       timesheet_class_id: '72e403d8-c36e-43a9-94b2-2820bcc1ec14',
-      timesheet_date: dateT,
+      timesheet_date: new Date(dateFormat),
     },
   })
 
